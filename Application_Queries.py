8008a595359dfeb690491ca83d4d4b2cb9ba8bac
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-03-02 00:35:09 +0200

    Update Application_Queries.py

--- Application_Queries.py
+++ Application_Queries.py
@@ -3,15 +3,16 @@
 '''
 
 import firebase_admin
+import os
 
 from firebase_admin import credentials
 from firebase_admin import db
+
 
 class FirebaseQueries:
     """
 
     """
-
 
     def maim(self):
         """
@@ -24,13 +25,28 @@ class FirebaseQueries:
         firebase_admin.initialize_app(cred, {"databaseURL": "https://cn-finalproject-default-rtdb.firebaseio.com/"})
 
     def add_new_student(self, student_data):
-		"""
+        """
 
         :return:
-		"""
-		dep = db.reference(student_data[0])
-		year = dep.child('year1')
-		year.update({
+        """
+        depart =  open("depart.txt", "ra")
+        contain = False
+        for x in depart:
+            if x == student_data[0]:
+                contain = True
+         
+        if contain == False:
+            depart.write(student_data[0])
+        
+        depart.close()
+        
+            
+        f = open(student_data[0] + ".txt", "a")
+        f.write(student_data[1] + "\n")
+        f.close()
+        dep = db.reference(student_data[0])
+        year = dep.child('year1')
+        year.update({
             student_data[1]: {
                 'info': {
                     'id': student_data[1],
@@ -48,12 +64,33 @@ class FirebaseQueries:
             }
         })
 
-def delete_existing_student(self, id):
+    def delete_existing_student(self, student_data):
         """
 
         :return:
         """
-        pass
+        delete = open(student_data[0] + "edit.txt", "a")
+        f = open(student_data[0] + ".txt", "r")
+        for line in f:
+            if line != student_data[0]:
+                delete.write(line + "\n")
+        
+        f.close()
+        f = open(student_data[0] + ".txt", "w")
+        f.write("")
+        f.close()
+        f = open(student_data[0] + ".txt", "a")
+        for line in delete:
+            delete.write(line + "\n")
+        
+        delete.close()
+        f.close()
+        if os.path.exists(student_data[0] + "edit.txt"):
+            os.remove(student_data[0] + "edit.txt")
+    
+        dep = db.reference(student_data[0])
+        year = dep.child(student_data[1])
+        year.child(student_data[2]).delete()
 
     def update_exsiting_student(self):
         """
@@ -67,21 +104,25 @@ def delete_existing_student(self, id):
 
         :return:
         """
-        pass
+        all_stud = db.reference().get()
+        print(all_stud)
 
-    def print_single_student(self):
+    def print_single_student(self , student_data):
         """
 
         :return:
         """
-        pass
+        dep = db.reference(student_data[0])
+        year = dep.child(student_data[1])
+        print(year.child(student_data[2]).get())
 
     def print_avg_student(self, type):
         """
 
         :return:
         """
-        pass
+
+
 
     def print_avg_of_avgs(self):
         """
@@ -103,3 +144,12 @@ def delete_existing_student(self, id):
         :return:
         """
         pass
+
+
+if __name__ == "__main__":
+    cred_obj = firebase_admin.credentials.Certificate('FireBase_SDK.json')
+    default_app = firebase_admin.initialize_app(cred_obj, {
+        'databaseURL': 'https://cn-finalproject-default-rtdb.firebaseio.com'
+    })
+    firebase = FirebaseQueries
+    FirebaseQueries.print_avg_student( firebase, 1)
