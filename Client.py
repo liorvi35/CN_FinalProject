0dfaf8fd1a1dd0548287686f7b95b4c8a9e4493e
liorvi35 <liorvi35@gmail.com>
2023-03-05 22:50:17 +0200

    Create Client.py

---
+++ Client.py
@@ -0,0 +1,16 @@
+"""
+this file is contains the client for the project
+Authors: Lior Vinman, Yoad Tamar
+Date: 05.03.2023
+"""
+
+# imports
+import Application_GUI
+
+
+if __name__ == "__main__":
+    """
+    here were calling the GUI 
+    """
+    gui = Application_GUI.GUI()
+    gui.main()
