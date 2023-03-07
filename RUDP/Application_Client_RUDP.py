fb2ebf09c70559a596b33555cd1b550a67e5cffe
liorvi35 <liorvi35@gmail.com>
2023-03-08 01:11:59 +0200

    Update Application_Client_RUDP.py

--- RUDP/Application_Client_RUDP.py
+++ RUDP/Application_Client_RUDP.py
@@ -10,23 +10,24 @@ class Client:
         self.seq_num = 0
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    def send(self, message):
-        packets = self.create_packets(message)
-        while True:
-            window = packets[self.seq_num:self.seq_num + self.window_size]
-            for i in range(len(window)):
-                packet = window[i]
-                self.socket.sendto(packet.encode(), (self.address, self.port))
-            try:
-                data, server_address = self.socket.recvfrom(self.buffer_size)
-            except socket.timeout:
-                continue
-            else:
-                seq_num, ack = data.decode().split('|')
-                if int(seq_num) >= self.seq_num and ack == 'ACK':
-                    self.seq_num = int(seq_num) + 1
-                    if self.seq_num >= len(packets):
-                        break
+    def send(self, messages):
+        for message in messages:
+            packets = self.create_packets(message)
+            while True:
+                window = packets[self.seq_num:self.seq_num + self.window_size]
+                for i in range(len(window)):
+                    packet = window[i]
+                    self.socket.sendto(packet.encode(), (self.address, self.port))
+                try:
+                    data, server_address = self.socket.recvfrom(self.buffer_size)
+                except socket.timeout:
+                    continue
+                else:
+                    seq_num, ack = data.decode().split('|')
+                    if int(seq_num) >= self.seq_num and ack == 'ACK':
+                        self.seq_num = int(seq_num) + 1
+                        if self.seq_num >= len(packets):
+                            break
 
     def create_packets(self, message):
         packets = []
@@ -38,4 +39,5 @@ class Client:
 
 if __name__ == '__main__':
     client = Client()
-    client.send('hello')
+    messages = ['hello', 'world', 'how are you?']
+    client.send(messages)
