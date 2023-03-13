309eba29dbbf5aa3cf5bf6c2aad0bca1c380706d
liorvi35 <liorvi35@gmail.com>
2023-03-13 17:20:00 +0200

    Update Application_GUI.py

--- Application_GUI.py
+++ Application_GUI.py
@@ -182,11 +182,128 @@ class GUI:
         self.cred.config(font=("MS Outlook", 10))
         self.cred.pack()
 
-    def conn_dhcp(self):
-        pass
+        def conn_dhcp(self):
+
+        exp_flag = False
+
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            sock.bind(DHCP_CLIENT_ADDR)
+        except (Exception, socket.error) as e:
+            messagebox.showerror("Error-Occurred", f"{e}")
+            exp_flag = True
+
+        data = self.discover_get()
+
+        try:
+            sock.sendto(data, DHCP_DEST)
+            data, address = sock.recvfrom(BUFFER_SIZE)
+        except (Exception, socket.error) as e:
+            messagebox.showerror("Error-Occurred", f"{e}")
+            exp_flag = True
+
+        data = self.request_get()
+
+        try:
+            sock.sendto(data, DHCP_DEST)
+            data, address = sock.recvfrom(BUFFER_SIZE)
+        except (Exception, socket.error) as e:
+            messagebox.showerror("Error-Occurred", f"{e}")
+            exp_flag = True
+
+        if not exp_flag:
+            messagebox.showinfo("Success", "DHCP configuration success!")
+
+    def discover_get(self):
+        OP = bytes([0x01])
+        HTYPE = bytes([0x01])
+        HLEN = bytes([0x06])
+        HOPS = bytes([0x00])
+        XID = bytes([0x39, 0x03, 0xF3, 0x26])
+        SECS = bytes([0x00, 0x00])
+        FLAGS = bytes([0x00, 0x00])
+        CIADDR = bytes([0x00, 0x00, 0x00, 0x00])
+        YIADDR = bytes([0x00, 0x00, 0x00, 0x00])
+        SIADDR = bytes([0x00, 0x00, 0x00, 0x00])
+        GIADDR = bytes([0x00, 0x00, 0x00, 0x00])
+        CHADDR1 = bytes([0x00, 0x05, 0x3C, 0x04])
+        CHADDR2 = bytes([0x8D, 0x59, 0x00, 0x00])
+        CHADDR3 = bytes([0x00, 0x00, 0x00, 0x00])
+        CHADDR4 = bytes([0x00, 0x00, 0x00, 0x00])
+        CHADDR5 = bytes(192)
+        Magiccookie = bytes([0x63, 0x82, 0x53, 0x63])
+        DHCPOptions1 = bytes([53, 1, 1])
+        DHCPOptions2 = bytes([50, 4, 0xC0, 0xA8, 0x01, 0x64])
+
+        package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR + YIADDR + SIADDR + GIADDR + CHADDR1 + CHADDR2 + CHADDR3 + CHADDR4 + CHADDR5 + Magiccookie + DHCPOptions1 + DHCPOptions2
+
+        return package
+
+    def request_get(self):
+        OP = bytes([0x01])
+        HTYPE = bytes([0x01])
+        HLEN = bytes([0x06])
+        HOPS = bytes([0x00])
+        XID = bytes([0x39, 0x03, 0xF3, 0x26])
+        SECS = bytes([0x00, 0x00])
+        FLAGS = bytes([0x00, 0x00])
+        CIADDR = bytes([0x00, 0x00, 0x00, 0x00])
+        YIADDR = bytes([0x00, 0x00, 0x00, 0x00])
+        SIADDR = bytes([0x00, 0x00, 0x00, 0x00])
+        GIADDR = bytes([0x00, 0x00, 0x00, 0x00])
+        CHADDR1 = bytes([0x00, 0x0C, 0x29, 0xDD])
+        CHADDR2 = bytes([0x5C, 0xA7, 0x00, 0x00])
+        CHADDR3 = bytes([0x00, 0x00, 0x00, 0x00])
+        CHADDR4 = bytes([0x00, 0x00, 0x00, 0x00])
+        CHADDR5 = bytes(192)
+        Magiccookie = bytes([0x63, 0x82, 0x53, 0x63])
+        DHCPOptions1 = bytes([53, 1, 3])
+        DHCPOptions2 = bytes([50, 4, 0xC0, 0xA8, 0x01, 0x64])
+        DHCPOptions3 = bytes([54, 4, 0xC0, 0xA8, 0x01, 0x01])
+
+        package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR + YIADDR + SIADDR + GIADDR + CHADDR1 + CHADDR2 + CHADDR3 + CHADDR4 + CHADDR5 + Magiccookie + DHCPOptions1 + DHCPOptions2 + DHCPOptions3
+
+        return package
 
     def conn_dns(self):
-        pass
+        dns_wind = tk.Toplevel(self.master)
+        dns_wind.geometry(f"{DNS_WIDTH}x{DNS_HEIGHT}")
+        dns_wind.title("Domain Name System")
+
+        dns_label = tk.Label(dns_wind, text="Enter Domain Name:")
+        dns_label.config(font=("MS Outlook", 15))
+        dns_label.pack(padx=20, pady=10)
+
+        self.dns_box = tk.Entry(dns_wind)
+        self.dns_box.pack(padx=20, pady=10)
+
+        self.dns_out = tk.Label(dns_wind, text="")
+        self.dns_out.pack(padx=20, pady=10)
+
+        dns_sub = tk.Button(dns_wind, text="Get Domain's IP-Address!", width=20, command=self.dns_submit)
+        dns_sub.config(font=("MS Outlook", 10, "bold"))
+        dns_sub.pack()
+
+    def dns_submit(self):
+        data = self.dns_box.get()
+
+        if data == "":
+            messagebox.showerror("Error-Occurred", "Enter non empty url!")
+        else:
+            try:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.connect(DNS_ADDR)
+                sock.sendall(data.encode())
+                data = sock.recv(BUFFER_SIZE).decode()
+                sock.shutdown(socket.SHUT_RDWR)
+                sock.close()
+            except Exception as e:
+                messagebox.showerror("Error-Occurred", "Cannot connect to DNS server!")
+
+        text = data if data == "Non-Existent Domain" else f"Domain's IP Address = {data}"
+
+        self.dns_out.config(text=text)
 
     def conn_app(self, protocol):
         """
@@ -699,7 +816,7 @@ class GUI:
         :param protocol: tcp or rudp
         """
         frame = tk.Toplevel(self.master)
-        frame.title("Delete Existing Student")
+        frame.title("Factor")
 
         label = tk.Label(frame, text="Number of points")
         label.grid(row=0, column=0, padx=10, pady=5, sticky="W")
